79b64c0a924928cc7c8a4c5a0413e594a452c723
muhurtkumar <muhurtkumar@gmail.com>
2024-11-22 00:35:26 +0530

    Update App.jsx
    Added infinite scroll bar to display more news removing previous and next buttons,
    Also added top loading bar to show the progress while reloading the page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import React, { Component } from 'react'
 import Navbar from './components/navbar'
 import News from './components/News'
+import LoadingBar from 'react-top-loading-bar'
+
 import {
     BrowserRouter as Router,
     Routes,
@@ -11,23 +13,36 @@ import {
   
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router>
-            <Navbar/>
-            <Routes>
-                <Route exact path="/" element={<News key="general" pageSize={20} country="us" category="general" />} />
-                <Route exact path="/Business" element={<News key="business" pageSize={20} country="us" category="business" />} />
-                <Route exact path="/Entertainment" element={<News key="entertainment" pageSize={20} country="us" category="entertainment" />} />
-                <Route exact path="/General" element={<News key="general" pageSize={20} country="us" category="general" />} />
-                <Route exact path="/Health" element={<News key="health" pageSize={20} country="us" category="health" />} />
-                <Route exact path="/Science" element={<News key="science" pageSize={20} country="us" category="science" />} />
-                <Route exact path="/Sports" element={<News key="sports" pageSize={20} country="us" category="sports" />} />
-                <Route exact path="/Technology" element={<News key="technology" pageSize={20} country="us" category="technology" />} />
-            </Routes>
-        </Router>
-      </div>
-    )
-  }
+    apiKey = import.meta.env.VITE_NEWS_API;
+
+    state = {
+        progress: 0
+    }
+    setProgress = (progress)=>{
+        this.setState({progress: progress})
+    }
+
+    render() {
+        return (
+        <div>
+            <Router>
+                <Navbar/>
+                <LoadingBar
+                    color='#f11946'
+                    progress={this.state.progress}
+                />
+                <Routes>
+                    <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={5} country="us" category="general" />} />
+                    <Route exact path="/Business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={5} country="us" category="business" />} />
+                    <Route exact path="/Entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={5} country="us" category="entertainment" />} />
+                    <Route exact path="/General" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={5} country="us" category="general" />} />
+                    <Route exact path="/Health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={5} country="us" category="health" />} />
+                    <Route exact path="/Science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={5} country="us" category="science" />} />
+                    <Route exact path="/Sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={5} country="us" category="sports" />} />
+                    <Route exact path="/Technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={5} country="us" category="technology" />} />
+                </Routes>
+            </Router>
+        </div>
+        )
+    }
 }
