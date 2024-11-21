db5e95590ff5323dfcfc74a8611df2bacc7c4d4a
muhurtkumar <muhurtkumar@gmail.com>
2024-11-22 00:33:53 +0530

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ import loading from './loading.gif'
 export default class Loading extends Component {
   render() {
     return (
-      <div className="d-flex justify-content-center align-items-center" style={{ height: "50vh" }}>
+      <div className="d-flex justify-content-center align-items-center" style={{ height: "10vh" }}>
         <img src={loading} alt="loading" />
       </div>
     )
