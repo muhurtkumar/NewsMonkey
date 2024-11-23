c35b60c05eda2f7882ec26e09fd488fe99e310ed
muhurtkumar <muhurtkumar@gmail.com>
2024-11-24 03:52:10 +0530

    Update NewsItem.jsx

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -6,7 +6,7 @@ export default class NewsItem extends Component {
     return (
       <div className="my-3" style={{position: "relative"}}>
         <span className="badge rounded-pill bg-danger" style={{ position: "absolute", top: "-10px", right: "-10px", zIndex: "1" }}>{source}</span>
-        <div className="card" style={{height: "480px", padding:"0", overflow: "hidden"}}>
+        <div className="card" style={{height: "480px", padding:"0", overflow: "hidden", backgroundColor: this.props.mode === 'light' ? 'white' : '#042743', color: this.props.mode === 'light' ? 'black' : 'white', border: `1px solid ${this.props.mode === 'light' ? 'black' : 'white'}`}}>
             <img src={!imageUrl?"https://img.freepik.com/vecteurs-premium/breaking-news-background-business-technologie-modele-breaking-news-texte-bleu-fonce-terre_258787-4554.jpg":imageUrl} className="card-img-top" alt="..." style={{height: "200px", objectFit: "cover"}}/>
             <div className="card-body" style={{height: "280px", overflow: "hidden"}}>
                 <h5 className="card-title" style={{height: "50px", textOverflow: "ellipsis", display: "-webkit-box", WebkitBoxOrient: "vertical", WebkitLineClamp: 2, overflow: "hidden"}}>{title}</h5>
