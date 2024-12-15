e1a8991a6abf26f9c88d1801c349f9b101547b83
muhurtkumar <muhurtkumar@gmail.com>
2024-12-16 00:19:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 import React, { Component } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import News from './components/News'
 import LoadingBar from 'react-top-loading-bar'
 
