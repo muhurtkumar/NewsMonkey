f0975ed5f4c1d3e49594c4fd5e5a0257cc496a0e
muhurtkumar <muhurtkumar@gmail.com>
2024-12-16 00:15:21 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {
     Link
   } from "react-router-dom";
 
-export default class navbar extends Component {
+export default class Navbar extends Component {
   render() {
     return (
       <div>
